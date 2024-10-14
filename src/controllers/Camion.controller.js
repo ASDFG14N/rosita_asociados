4c156522f853d5010a75aa2bd0fca238ed87f1b1
Adenae <leoapariramirez3@gmail.com>
2024-10-14 11:29:18 -0500

    asdasd

--- src/controllers/Camion.controller.js
+++ src/controllers/Camion.controller.js
@@ -2,6 +2,7 @@ import { getConnection, sql } from "../database/connection.js";
 
 export const getCamiones = async (req, res) => {
   try {
+
     const pool = await getConnection();
     const result = await pool.request().query("SELECT * FROM Camion");
     res.json(result.recordset);
@@ -11,33 +12,28 @@ export const getCamiones = async (req, res) => {
   }
 };
 
-//CREACION DE UN NUEVO REGISTRO
+
 export const createNewCamion = async (req, res) => {
-  //DENTRO DEL reqUEST SACAMOS LOS VALORES A USAR DENTRO DEL BODY
-  const { placa, modelo} = req.body;
-//VERIFICAMOS QUE NO SEA NULO LOS INPUTS
-  if (placa == null || modelo == null) {
+  const { Placa, Modelo} = req.body;
+  
+
+  if (Placa == null || Modelo == null) {
     return res.status(400).json({ msg: "Bad Request. Please fill all fields" });
   }
-  //INICIAMOS EL TRY CATCH
   try {
-    //INCIAMOS LA CONEXION
     const pool = await getConnection();
 
-    /*COMENZAMOS A TRANSFERIR AL pool LOS DATOS QUE SACAMOS DEL BODY Y VERIFICAMOS QUE SEAN
-    DEL TIPO QUE NECESITAMOS CON EL sql */
     const result = await pool
       .request()
-      .input("placa", sql.VarChar, placa)
-      .input("modelo ", sql.VarChar, modelo)
+      .input("Placa", sql.VarChar, Placa)
+      .input("Modelo", sql.VarChar, Modelo)
       .query(
-        "INSERT INTO Camion (placa, modelo) VALUES (@name,@description); SELECT SCOPE_IDENTITY() as id"
+        "INSERT INTO Camion (Placa, Modelo) VALUES (@Placa,@Modelo); SELECT SCOPE_IDENTITY() as id"
       );
 
-    //AL FINALIZAR EL pool CON EL query ENVIAMOS COMO RESPUESTA ESTE JSON
     res.json({
-      placa,
-      modelo
+      Placa,
+      Modelo
     });
   } catch (error) {
     res.status(500);
@@ -80,17 +76,18 @@ export const deleteCamionByPlaca = async (req, res) => {
 };
 
 export const getTotalCamion = async (req, res) => {
+  
   const pool = await getConnection();
   const result = await pool.request().query("SELECT COUNT(*) FROM Camion");
   res.json(result.recordset[0][""]);
 };
 
 export const updateCamionByPlaca = async (req, res) => {
-  const { placa, modelo} = req.body;
+  const { Placa, Modelo} = req.body;
 
   if (
-    placa == null ||
-    modelo == null
+    Placa == null ||
+    Modelo == null
   ) {
     return res.status(400).json({ msg: "Bad Request. Please fill all fields" });
   }
@@ -99,15 +96,15 @@ export const updateCamionByPlaca = async (req, res) => {
     const pool = await getConnection();
     const result = await pool
       .request()
-      .input("placa", sql.VarChar, placa)
-      .input("modelo ", sql.VarChar, modelo)
+      .input("Placa", sql.VarChar, Placa)
+      .input("Modelo", sql.VarChar, Modelo)
       .query(
-        "UPDATE Camion SET Modelo = @modelo WHERE Placa = @placa"
+        "UPDATE Camion SET Modelo = @Modelo WHERE Placa = @Placa"
       );
 
     if (result.rowsAffected[0] === 0) return res.sendStatus(404);
 
-    res.json({ placa, modelo});
+    res.json({ Placa, Modelo});
   } catch (error) {
     res.status(500);
     res.send(error.message);
