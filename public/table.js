4b835edc374369873e2b91bed5966f61de8ad916
ASDFG14N <128514490+ASDFG14N@users.noreply.github.com>
2024-10-17 10:00:42 -0500

    rendered table

--- public/table.js
+++ public/table.js
@@ -1,3 +1,5 @@
+const URL_API = "https://jsonplaceholder.typicode.com/users";
+
 const generateHeaders = (headers) => {
   let headerHTML = `<th scope="col" class="px-6 py-3 w-[2rem]">ID</th>`;
   headers.forEach((header) => {
@@ -8,31 +10,40 @@ const generateHeaders = (headers) => {
   headersRow.innerHTML = headerHTML;
 };
 
-const fillTableBody = async () => {
+const fillTableBody = async (numberOfFields) => {
   try {
-    const response = await fetch("URL");
+    const response = await fetch(URL_API);
     if (!response.ok) {
       throw new Error(`Error: ${response.status}`);
     }
+
     const data = await response.json();
     const tableBody = document.getElementById("table-body");
     tableBody.innerHTML = "";
+
     data.forEach((item) => {
-      const row = `
-        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-            <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-              4
+      let row = `
+        <tr class="border-b bg-slate-500 dark:border-gray-700 hover:bg-gray-600">
+            <td class="px-6 py-4 font-medium text-black whitespace-nowrap">
+              ${item.id}
             </td>
-            <td class="px-6 py-4">Silver</td>
-            <td class="px-6 py-4">Laptop</td>
+      `;
+
+      const itemValues = Object.values(item);
+
+      for (let i = 1; i <= numberOfFields && i < itemValues.length; i++) {
+        row += `<td class="px-6 py-4 text-black">${itemValues[i]}</td>`;
+      }
+      row += `
             <td class="px-6 py-4 text-right w-[2rem]">
-              <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Editar</a>
+              <a href="#" class="font-medium text-gray-900 hover:underline edit">Editar</a>
             </td>
             <td class="px-6 py-4 text-right w-[2rem]">
-              <a href="#" class="font-medium text-red-600 dark:text-red-500 hover:underline">Remove</a>
+              <a href="#" class="font-medium text-red-800 hover:underline" id="del">Remove</a>
             </td>
         </tr>
       `;
+
       tableBody.innerHTML += row;
     });
   } catch (error) {
@@ -42,7 +53,45 @@ const fillTableBody = async () => {
 
 const headers = sessionStorage.getItem("tableHeaders");
 const headersRow = document.getElementById("headers");
+const closeButton = document.getElementById("close");
+const form = document.getElementById("form-edit");
 
 if (headers) {
-  generateHeaders(headers.split(";"));
+  const headersList = headers.split(";");
+  generateHeaders(headersList);
+  (async () => {
+    await fillTableBody(3);
+    const elements = document.getElementsByClassName("edit");
+    const edit = document.getElementById("container-edit");
+    const toggleButton = elements[0];
+
+    toggleButton.addEventListener("click", () => {
+      edit.classList.remove("hidden");
+    });
+    closeButton.addEventListener("click", () => {
+      edit.classList.add("hidden");
+    });
+    let htmlForm;
+    headersList.forEach((header) => {
+      htmlForm += `
+      <div class="mb-4">
+        <label class="block text-gray-300 text-sm font-bold mb-2" for="username">
+          ${header}
+        </label>
+        <input
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          type="text">
+      </div>
+      `;
+    });
+    htmlForm += `   
+      <div class="flex items-center justify-end">
+        <button
+          class="bg-black text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="button">
+          Confirmar
+        </button>
+      </div>`;
+    form.innerHTML = htmlForm;
+  })();
 }
